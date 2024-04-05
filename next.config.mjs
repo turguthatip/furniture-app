b2ef0448260470648ec11fa56466210cc0693145
turguthatip <turguthatip@gmail.com>
2024-04-05 20:24:14 +0300

    Your commit message here

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-
+    reactStrictMode: true,
+    images: {
+      loader: "akamai",
+      path: "/",
+    }
 };
 
 export default nextConfig;
