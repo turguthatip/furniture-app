3438e687dc61f080082452c2976566cb1dd71311
turguthatip <turguthatip@gmail.com>
2024-04-05 20:30:15 +0300

    Your commit message here

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Slider from 'react-slick';
 import Slide from './Slide';
+import Image from 'next/image';
 
 const Hero = () => {
 
--- components/NewProducts.tsx
+++ components/NewProducts.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './Card';
+import Image from 'next/image';
 
 const productData = [
     {
