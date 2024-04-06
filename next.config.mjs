a659c70556f02c6ee63e50e6b1adee4ad51c989a
turguthatip <turguthatip@gmail.com>
2024-04-06 16:14:27 +0300

    update the README.md with link to contributing guide

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    basePath: '/https://turguthatip.github.io/furniture-app/',
     images: {
         loader: 'akamai',
         path: './',
