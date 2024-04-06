e4e2eeb5b6faf8ea194e21441be96a80ff02538e
turguthatip <turguthatip@gmail.com>
2024-04-06 13:50:54 +0300

    update the README.md with link to contributing guide

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Header = () => {
   return (
     <nav className="border-b boder-gray-200 py-4 px-6 flex items-center justify-center">
