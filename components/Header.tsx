0bf7cd3efb8ab65dd638eb0b19ecbd9b9508903c
turguthatip <turguthatip@gmail.com>
2024-04-06 14:31:42 +0300

    update the README.md with link to contributing guide

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+
 const Header = () => {
   return (
     <nav className="border-b boder-gray-200 py-4 px-6 flex items-center justify-center">
     <a href="#">
-    <img src="/assuva-logo.png" alt="Assuva Logo" className="h-16" />
+    <img src="./assuva-logo.png" alt="Assuva Logo" className="h-16" />
 
     </a>
   </nav>
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        loader: "akamai",
-        path: "/",
-      },
+    basePath: '/furniture.app'
+
 };
 
 export default nextConfig;
