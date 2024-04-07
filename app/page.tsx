1afcc51c01943b96d38b00f7674ee55323021d99
turguthatip <turguthatip@gmail.com>
2024-04-07 15:56:22 +0300

    Google Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import NewProducts from "@/components/NewProducts";
 import Image from "next/image";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 export default function Home() {
