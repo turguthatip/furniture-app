e28b837a3eaadb771e487d163668bf49d99a069a
turguthatip <turguthatip@gmail.com>
2024-04-08 12:20:33 +0300

    next-sitemap config editted

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-    siteUrl:'https://assuva.com.tr/',
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    sitemapSize: 1000,
-  }
-  
-  export default config
+// eslint-disable-next-line no undef
+
+module.exports = {
+  siteUrl: 'https://assuva.com.tr',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
