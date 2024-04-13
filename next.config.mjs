96b7a0ca8ce0b87da16997cfbc80e08257377d60
turguthatip <turguthatip@gmail.com>
2024-04-13 12:11:39 +0300

    output:export didnt work

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+// @ts-check
+ 
+export default (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+            /* config options here */
+        output: 'export',
+        }
+    return nextConfig
+  }
